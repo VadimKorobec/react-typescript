5c118c3f71ce10299fca1e577fa0231bbd4060a4
VadimKorobec <_s_e_t__@ukr.net>
2023-08-27 16:23:31 +0300

    add User.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
+import { Box } from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
 import { Counter } from "./components/state/Counter";
 
 function App() {
@@ -43,6 +45,9 @@ function App() {
         }}
       />
       <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
